59a41b570d11e794a27b005e864409c9c6524be2
Thiago Vanzele <thiagovanzele@gmail.com>
2026-03-22 01:56:36 -0300

    mudança porta frontend para 5173

--- backend/src/main/java/br/com/vanzele/encurtador/config/CorsConfig.java
+++ backend/src/main/java/br/com/vanzele/encurtador/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // seu React
+                        .allowedOrigins("http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
